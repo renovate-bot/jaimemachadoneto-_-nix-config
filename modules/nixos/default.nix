8e094758003ec92ffe5bc5c875db7c07741e8da4
John Doe <johndoe@example.com>
2025-01-30 17:21:17 +0100

    Adding User JAime

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -8,7 +8,7 @@ let
 in
 {
   # These users can add Nix caches.
-  nix.settings.trusted-users = [ "root" "runner" ];
+  nix.settings.trusted-users = [ "root" "runner" "jaime" ];
 
   services.openssh.enable = true;
 }
