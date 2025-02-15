7f08bea43c67831e8130015fde55ceba9804eb3a
Jaime Machado <jaime.machado@gmail.com>
2025-02-15 16:10:31 +0100

    Add NixOS configuration files for host specifications and system setup

---
+++ config.nix
@@ -0,0 +1,9 @@
+# Configuration for this repo
+# See ./modules/flake-parts/config.nix for module options.
+{
+  hostSpec = {
+    username = "jaime";
+    userFullname = "Jaime Machado";
+    email = "jaime.machado@gmail.com";
+  };
+}
