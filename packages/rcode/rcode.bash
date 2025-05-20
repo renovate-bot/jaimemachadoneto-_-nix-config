a3fe8e7b5bf8f03bd4f961df95eda49f48817b09
Jaime Machado <jaime.machado@gmail.com>
2025-05-20 10:37:22 +0200

    Fix quoting issues in rcode script to improve reliability

--- packages/rcode/rcode.bash
+++ packages/rcode/rcode.bash
@@ -3,19 +3,24 @@
 max_retry=10
 
 for i in $(seq 1 $max_retry); do
+    # shellcheck disable=SC2012
     recent_folder=$(ls -d ~/.vscode-server/cli/servers/*/ -t | head -n1 | tail -1)
-    script=$(echo $recent_folder/server/bin/remote-cli/code)
+    # shellcheck disable=SC2116
+    script=$(echo "$recent_folder/server/bin/remote-cli/code")
     if [[ -z ${script} ]]; then
         echo "VSCode remote script not found"
         exit 1
     fi
+    # shellcheck disable=SC2012
+    # shellcheck disable=SC2086
     socket=$(ls /run/user/$UID/vscode-ipc-* -t | head -n$i | tail -1)
     if [[ -z ${socket} ]]; then
         echo "VSCode IPC socket not found"
         exit 1
     fi
     export VSCODE_IPC_HOOK_CLI=${socket}
-    ${script} $@ 2>/dev/null
+    ${script} "$@" 2>/dev/null
+    # shellcheck disable=SC2181
     if [ "$?" -eq "0" ]; then
         exit 0
     fi
